a8006ddede1c713bba67e11dd898d9d904f2d50b
anonymoussc <50c5ac69@opayq.com>
2017-07-27 11:53:35 +0700

    Updated \tasks\release

--- app-client/ionic/tasks/release/release.js
+++ app-client/ionic/tasks/release/release.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
-const gulp = require('gulp')
+const gulp = require('gulp');
 const utils = require('../utils')
-
+;
 const releaseForOs = {
   osx: require('./osx'),
   linux: require('./linux'),
   windows: require('./windows')
-}
+};
 
 gulp.task('release', ['build'], function () {
   return releaseForOs[utils.os()]()
-})
+});
