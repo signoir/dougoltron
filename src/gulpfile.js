f9797142f88863ed392356e87364b99f3a4d19cb
anonymoussc <50c5ac69@opayq.com>
2017-11-05 00:03:57 +0700

    Modified gulpfile src

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,17 +1,17 @@
-const gulp     = require('gulp');
-const electron = require('electron-connect').server.create();
-const env      = require('gulp-env');
-const config   = require('@ionic/app-scripts/dist/util/config');
-const ionic    = require('@ionic/app-scripts');
+const gulp = require('gulp')
+const electron = require('electron-connect').server.create()
+const env = require('gulp-env')
+const config = require('@ionic/app-scripts/dist/util/config')
+const ionic = require('@ionic/app-scripts')
 
 gulp.task('electron-live', function () {
   env.set({
     NODE_ENV: 'development'
-  });
+  })
   // Start browser process
-  electron.start();
+  electron.start()
 
-  gulp.watch('src/app.js', electron.restart);
+  gulp.watch('src/app.js', electron.restart)
 
   // Reload renderer process
   gulp.watch([
@@ -19,14 +19,14 @@ gulp.task('electron-live', function () {
     'www/**/*.html',
     'www/**/*.css',
     'www/**/*.js'], electron.reload)
-});
+})
 
 gulp.task('dev', function () {
   ionic.watch(config.generateContext())
-    .then(function () {
-      gulp.start('electron-live')
-    })
-    .catch(function (err) {
-      console.log('Error starting watch: ', err)
-    })
-});
+  .then(function () {
+    gulp.start('electron-live')
+  })
+  .catch(function (err) {
+    console.log('Error starting watch: ', err)
+  })
+})
