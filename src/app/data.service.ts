00bbf2233b46c07527a75d7114fd601a34658ba9
anonymoussc <50c5ac69@opayq.com>
2019-06-09 13:40:34 +0700

    feat(app): modified ./app/data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,7 +3,7 @@ import { Platform, Events } from '@ionic/angular'
 import { environment } from '../environments/environment';
 import { ElectronService } from 'ngx-electron'
 import * as PouchDB from 'pouchdb/dist/pouchdb'
-import SimpleCryptor from 'simple-cryptor-pouch'
+import SecurePouch from 'polyonic-secure-pouch'
 import cordovaSqlitePlugin from 'pouchdb-adapter-cordova-sqlite'
 
 @Injectable({
@@ -20,51 +20,11 @@ export class DataService {
     const ctx = this
     console.log('Setting up the application database')
 
-    if (ctx.electron.isElectronApp) {
-      return ctx.desktopDB()
-    }
     if (ctx.platform.is('mobile')) {
       return ctx.mobileDB()
     } else {
       return ctx.webDB()
     }
-  }
-
-  private desktopDB() {
-    const ctx = this
-    console.log('This app is running on the desktop')
-    return new Promise((resolve, reject) => {
-      console.log('Running Electron:', ctx.electron)
-      try {
-
-        const ElectronPouchDB = ctx.electron.remote.require('pouchdb')
-        const ElectronSimpleCryptor = ctx.electron.remote.require('simple-cryptor-pouch')
-        ElectronPouchDB.plugin(ElectronSimpleCryptor)
-
-        let userDataPath = '.'
-        if (environment.production) {
-          userDataPath = (ctx.electron.remote.app).getPath('userData')
-          console.log(`Application database is located in the application user path: ${userDataPath}`)
-        }
-
-        ctx.db = new ElectronPouchDB(userDataPath + '/app.db')
-        ctx.db.simplecryptor('secret') // <<<<<<<<<<<<< Replace with your secret key
-
-        // The app database won't be created until the api is called
-        // This can cause issues on a fresh production build
-        // Therefore, call db.info and then resolve to ensure the
-        // database exists
-        ctx.db.info()
-        .then(res => {
-          console.log(JSON.stringify(res, null, 2))
-          resolve()
-        })
-        .catch(error => reject(error))
-
-      } catch (error) {
-        reject(error)
-      }
-    })
   }
 
   private mobileDB() {
@@ -81,9 +41,9 @@ export class DataService {
           key: 'secret', // <<<<<<<<<<<<< Replace with your secret key
           iosDatabaseLocation: 'Documents'
         })
-        // PouchDB.plugin(SimpleCryptor)
+        // PouchDB.plugin(SecurePouch)
         // ctx.db = new PouchDB('app.db')
-        // ctx.db.simplecryptor('secret') // <<<<<<<<<<<<< Replace with your secret key
+        // ctx.db.securePouch('secret') // <<<<<<<<<<<<< Replace with your secret key
       })
       .then(res => {
         return ctx.db.info()
@@ -105,12 +65,12 @@ export class DataService {
     return new Promise((resolve, reject) => {
       ctx.platform.ready()
       .then(() => {
-        PouchDB.plugin(SimpleCryptor)
+        PouchDB.plugin(SecurePouch)
         return ctx.db = new PouchDB('app.db')
 
       })
       .then(res => {
-        ctx.db.simplecryptor('password') // <<<<<<<<<<<<< Replace with your secret key
+        ctx.db.encrypt('password') // <<<<<<<<<<<<< Replace with your secret key
         return ctx.db.info()
       })
       .then(info => {
